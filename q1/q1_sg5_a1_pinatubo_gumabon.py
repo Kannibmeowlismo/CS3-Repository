37df204e775d50079a93965bd606ee744d878564
Kannibmeowlismo <ksgumabon2030@clc.pshs.edu.ph>
2026-09-17 23:11:29 +0800

    Add files via upload

---
+++ q1/q1_sg5_a1_pinatubo_gumabon.py
@@ -0,0 +1,16 @@
+class Hero:
+    def __init__(self, name, hp):
+        self.name = name
+        self.hp = hp
+
+    def take_damage(self, amount):
+        self.hp -= amount
+
+
+Arthur = Hero("Arthur", 100)
+Morgana = Hero("Morgana", 100)
+
+Arthur.take_damage(10)
+
+print("Arthurs's HP:",Arthur.hp)
+print("Moranga's HP:", Morgana.hp)
